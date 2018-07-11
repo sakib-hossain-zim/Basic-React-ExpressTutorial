6a367787bc18124c29caf7fe24dbdf93b290fda7
sakib-hossain-zim <sakib_hossain@DESKTOP-VNS6AG8.localdomain>
2018-07-11 16:07:39 -0700

    Backend working fine with database connection

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,22 +1,49 @@
 var express = require('express');
+var mysql = require('mysql');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  // res.send('respond with a resource');
 
-  //Inserting some data to check
+var sqlquery = 'SELECT * FROM event';
 
-  res.json([{
-    traveller_id: 075,
-    username: "sakib_hossain"
-  },
-  {
-    traveller_id: 0100,
-    username: "hossain"
 
-  }
-]);
+// /* GET users listing. */
+//  router.get('/', function(req, res, next) {
+//    res.send('respond with a resource');
+//  });
+
+var printer;
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Topoftheworld10!',
+  database: 'travelplanmysqlserverdatabase'
 });
+
+connection.connect(err => {
+  if(err){
+    return err;  }
+});
+
+console.log(connection);
+
+console.log("Now here");
+router.get('/event/view',(req,res) => {
+  connection.query(sqlquery,(err,results) => {
+    if(err){
+      return res.send(err);
+      console.log(err)
+    } else {
+      return res.json({
+        data: results
+
+      })
+
+
+    }
+
+  });
+});
+
 
 module.exports = router;
